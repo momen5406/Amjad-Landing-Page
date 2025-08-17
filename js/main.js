4b849f00bf323751dac8d3c227251e856b054a65
momen5406 <morenocoder@gmail.com>
2025-08-17 22:52:37 +0300

    Edited nav

--- js/main.js
+++ js/main.js
@@ -1,17 +1,32 @@
 const menuBTN = document.querySelector("#menuBtn");
 const exitBTN = document.querySelector("#exitBtn");
 const menu = document.querySelector("#menu");
+const menuLinks = document.querySelectorAll("#menu a");
 
-menuBTN.addEventListener("click", () => {
+function openMenu() {
   menu.classList.remove("-translate-y-full");
   setTimeout(() => {
     exitBTN.classList.remove("hidden");
   }, 300);
-});
+}
 
-exitBTN.addEventListener("click", () => {
+function closeMenu() {
   menu.classList.add("-translate-y-full");
   setTimeout(() => {
     exitBTN.classList.add("hidden");
   }, 300);
+}
+
+menuBTN.addEventListener("click", () => {
+  openMenu();
+});
+
+exitBTN.addEventListener("click", () => {
+  closeMenu();
+});
+
+menuLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    closeMenu();
+  });
 });
